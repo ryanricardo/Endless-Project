0ac51659a6aa8e9992a731bae6ac6b3921826008
Ryan Ricardo Rossi <70291872+ryanricardo@users.noreply.github.com>
2022-11-10 21:07:21 -0300

    menu with buttons

---
+++ Assets/Scripts/MenuCanvas.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuCanvas : MonoBehaviour
+{
+
+    [Header("Components")]
+    [SerializeField] protected Button buttonContinue;
+    [SerializeField] protected Button buttonNewGame;
+    [SerializeField] protected Button buttonQuit;
+
+    void Start()
+    {
+        if(PlayerPrefs.GetInt("firstPlayed") == 0)
+        {
+            buttonContinue.interactable = false;
+        }else 
+        {
+            buttonContinue.interactable = true;
+        }
+
+        buttonContinue.onClick.AddListener(delegate
+        {
+            SceneManager.LoadScene("SampleScene");
+        });
+
+        buttonNewGame.onClick.AddListener(delegate
+        {
+            PlayerPrefs.DeleteAll();
+            SceneManager.LoadScene("SampleScene");
+
+        });
+
+        buttonQuit.onClick.AddListener(delegate
+        {
+            Application.Quit();
+        });
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
